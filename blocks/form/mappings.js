8242a6e66fdd283e139b088da5f1aa649ee86ae8
John Green <jwgreen@adobe.com>
2024-06-20 17:19:59 -0400

    Initial Commit

---
+++ blocks/form/mappings.js
@@ -0,0 +1,21 @@
+import range from './components/range/range.js';
+/**
+ * returns a decorator to decorate the field definition
+ *
+ * */
+export default async function componentDecorator(fd) {
+  const { ':type': type = '', fieldType } = fd;
+  if (fieldType === 'file-input') {
+    const module = await import('./components/file.js');
+    return module.default;
+  }
+  if (type.endsWith('wizard')) {
+    const module = await import('./components/wizard.js');
+    return module.default;
+  }
+
+  if (fieldType === 'range') {
+    return range;
+  }
+  return null;
+}
